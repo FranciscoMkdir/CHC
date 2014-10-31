872170c0605a96cd1d577c80c6edab2696aa04ac
Francisco Javier Delgado Garcia <franciscodelgadogarcia@MacBook-Air-de-Francisco.local>
2014-10-30 20:56:19 -0600

    Avance en la implementacion CHC. 	* Generar primera poblacion (aleatoriamente) 	* Evaluacion del cromosoma inicial 	* Evaluacion de la poblacion

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,133 @@
 import math
+import random
+
+class CHC:
+
+    def __init__(self, archivo):
+
+        f = open(archivo)
+
+        self.num = int(f.readline())
+        print ("Numero instancias: " + str(self.num))
+
+        self.tip = f.readline()
+        self.tip = self.tip.strip().split('\t')
+        print("Tipo de atributos ")
+        print(self.tip)
+        f.readline()
+
+        self.lines = f.readlines()
+        f.close()
+
+        self.crom_ini = []
+        for i in range(self.num):
+            self.crom_ini.append(1)
+
+        print ("Cromosoma inicial ")
+        print (self.crom_ini)
+
+
+
+    def init_P(self, t):
+
+        print("Poblacion Inicial ")
+        self.pob = {}
+        crom = []
+        self.tam = t
+
+        for i in range(self.tam):
+            for e in range(self.num):
+                crom.append(random.randrange(0,2))
+            self.pob[i] = crom
+            crom = []
+
+
+        print (self.pob)
+        return self.pob
+
+
+    #Evalua a la poblacion
+    def eval_P(self, p):
+        self.eval = {}
+        form = ""
+
+        for e in range(len(self.tip[:-1])):
+            if self.tip[e] == '0':
+                form += 'num' + '\t'
+            else:
+                form += 'attr' + '\t'
+        form += 'class'
+
+        print("Formato: " + form)
+
+
+
+
+
+        c_i = 0
+        for ip in range(len(p)):
+
+            f_aux = open("aux.txt","w")
+
+            c_e = 0
+            v = p[c_i]
+            for line in self.lines:
+                if v[c_e] == 1:
+                    f_aux.write(line)
+                c_e += 1
+
+            f_aux.close()
+            fit = Clasificador_Bayes("aux.txt", form)
+            r = fit.evaluar("T_V")
+
+            self.eval[c_i] = r
+            c_i += 1
+
+
+
+
+        print("Evaluaciones")
+        print (self.eval)
+
+
+    #Evalua cromosoma inicial
+    def eval_Crom(self):
+        self.eval = {}
+        form = ""
+
+        for e in range(len(self.tip[:-1])):
+            if self.tip[e] == '0':
+                form += 'num' + '\t'
+            else:
+                form += 'attr' + '\t'
+        form += 'class'
+
+        print("Formato: " + form)
+
+        f_aux = open("aux.txt","w")
+        for line in self.lines:
+            f_aux.write(line)
+
+        f_aux.close()
+
+        fit = Clasificador_Bayes("aux.txt", form)
+        fit.evaluar("T_V")
+
+
+
+
+
+    #Seleccion elitista
+    def sel_eti(self, p, h):
+        pass
+
+
+
+
+
+
+
+
 
 
 class Clasificador_Bayes:
@@ -120,9 +249,12 @@ class Clasificador_Bayes:
 
         clases = []
         aciertos = []
-        c = 0;
 
         f = open(archivo)
+
+        self.tip = f.readline()
+        self.tip = self.tip.strip().split('\t')
+
         lines = f.readlines()
         f.close()
 
@@ -136,37 +268,64 @@ class Clasificador_Bayes:
         print (clases)
 
 
-        vector = []
+        vector_num = []
+        vector_nom = []
+
+        self.tip = self.tip[:-1]
+
         for line in lines:
             l = line.strip().split('\t')
             l = l[:-1]
+            c = 0
             for e in l:
-                vector.append(float(e))
-            res = self.clasificar([], vector)
+                if (self.tip[c] == '0'):
+                    vector_num.append(float(e))
+                else:
+                    vector_nom.append(e)
+                c += 1
+
+            res = self.clasificar(vector_nom, vector_num)
             aciertos.append(res)
-            vector = []
+            vector_num = []
+            vector_nom = []
+
+
+
+        print(aciertos)
+
 
         #Calcular porcentaje de aciertos
+        c = 0
         for e in range(len(clases)):
             if (clases[e] == aciertos[e]):
                 c += 1
         #Regla se tres
         por = (c * 100) / len(clases)
         print"Porcentaje de aciertos: ", str(por)
+        return por
+
+#c = Clasificador_Bayes("datos", "attr\tattr\tattr\tclass")
+#print(c.clasificar(['health', 'moderate', 'moderate'], []))
+#print(c.clasificar(['both', 'sedentary', 'moderate'], []))
 
 
+#c2 = Clasificador_Bayes("pima_e", "num\tnum\tnum\tnum\tnum\tnum\tnum\tnum\tclass")
+#print("Clase: " + str(c2.clasificar([], [3, 78, 50, 32, 88, 31.0, 0.248, 26])))
+#print("Clase: " + str(c2.clasificar([], [2, 197, 70, 45, 543, 30.5, 0.158, 53])))
+#print("Clase: " + str(c2.clasificar([], [3, 78, 50, 32, 88, 31.0, 0.248, 26])))
+#print("Clase: " + str(c2.clasificar([], [1, 91, 54, 25, 100, 25.2, 0.234, 23])))
 
+#c2.evaluar("pima_v")
+#c2.evaluar("pima_v2")
 
-c = Clasificador_Bayes("datos", "attr\tattr\tattr\tclass")
-print(c.clasificar(['health', 'moderate', 'moderate'], []))
-print(c.clasificar(['both', 'sedentary', 'moderate'], []))
+#c3 = Clasificador_Bayes("prueba", "num\tnum\tnum\tnum\tnum\tnum\tnum\tnum\tclass")
+#c3.evaluar("pima_v3")
 
+#c4 = Clasificador_Bayes("pima_e2", "num\tnum\tnum\tnum\tnum\tnum\tnum\tnum\tattr\tclass")
+#print("Clase: " + str(c4.clasificar(['no'], [1, 91, 54, 25, 100, 25.2, 0.234,  23])))
+#print("Clase: " + str(c4.clasificar(['yes'], [2, 197, 70, 45, 543, 30.5, 0.158, 53])))
 
-c2 = Clasificador_Bayes("pima_e", "num\tnum\tnum\tnum\tnum\tnum\tnum\tnum\tclass")
-print("Clase: " + str(c2.clasificar([], [3, 78, 50, 32, 88, 31.0, 0.248, 26])))
-print("Clase: " + str(c2.clasificar([], [2, 197, 70, 45, 543, 30.5, 0.158, 53])))
-print("Clase: " + str(c2.clasificar([], [3, 78, 50, 32, 88, 31.0, 0.248, 26])))
-print("Clase: " + str(c2.clasificar([], [1, 91, 54, 25, 100, 25.2, 0.234, 23])))
-
-c2.evaluar("pima_v")
-c2.evaluar("pima_v2")
+g = CHC("T")
+p = g.init_P(10)
+g.eval_Crom()
+g.eval_P(p)
